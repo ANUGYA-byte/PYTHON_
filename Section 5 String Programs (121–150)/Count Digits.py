32ce3b92a1f7e76fd0f669dba249787f6545c5bb
ANUGYA-byte <304574911+ANUGYA-byte@users.noreply.github.com>
2026-08-18 22:06:04 +0530

    update

--- Section 5 String Programs (121–150)/Count Digits.py
+++
@@ -1,8 +0,0 @@
-STRING=input("ENTER THE STRING WHICH LENGTH NEED TO BE FIND-")
-COUNT=0
-for i in STRING:
-    if i in '1234567890':
-        COUNT=COUNT+1
-    else:
-        COUNT=COUNT+0
-print('Digits Count-',COUNT)
