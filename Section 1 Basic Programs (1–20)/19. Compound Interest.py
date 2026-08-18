2654bc53bc695a453693062abaca21a0168a989d
ANUGYA-byte <304574911+ANUGYA-byte@users.noreply.github.com>
2026-08-18 21:19:16 +0530

    Covers Python fundamentals including variables, data types, input/output, operators, and basic problem-solving.

--- Section 1 Basic Programs (1–20)/19. Compound Interest.py
+++ Section 1 Basic Programs (1–20)/19. Compound Interest.py
@@ -1,28 +1,21 @@
 # ---------------------------------------
 # Program 19: Compound Interest
-# Description: Calculates compound interest using principal, rate, and time.
+# Description: Calculates compound interest.
 # Author: Anugya Agrawal
 # ---------------------------------------
 
 principal = float(input("ENTER PRINCIPAL AMOUNT: "))
-rate = float(input("ENTER RATE OF INTEREST: "))
-time = float(input("ENTER TIME (YEAR): "))
-n_time = int(input("ENTER NUMBER OF TIMES INTEREST IS COMPOUNDED PER YEAR: "))
-if n_time ==0:
-    print("COMPOUND INTEREST -", 0)
+rate = float(input("ENTER RATE OF INTEREST (%): "))
+time = float(input("ENTER TIME (YEARS): "))
+n = int(input("ENTER NUMBER OF TIMES COMPOUNDED PER YEAR: "))
+
+if principal < 0 or rate < 0 or time < 0:
+    print("INVALID INPUT")
+elif n <= 0:
+    print("NUMBER OF COMPOUNDING TIMES MUST BE GREATER THAN 0")
 else:
-   amount = principal * (1 + rate / (100 * n_time)) ** (n_time * time)
-   compound_interest = amount - principal
+    amount = principal * (1 + rate / (100 * n)) ** (n * time)
+    compound_interest = amount - principal
 
-   print("COMPOUND INTEREST -", compound_interest)
-
-# ---------------------------------------
-# SAMPLE INPUT-
-# ENTER PRINCIPAL AMOUNT: 2000
-# ENTER RATE OF INTEREST: 4
-# ENTER TIME (YEAR): 2
-# ENTER NUMBER OF TIMES INTEREST IS COMPOUNDED PER YEAR: 4
-# ---------------------------------------
-# SAMPLE OUTPUT-
-# COMPOUND INTEREST - 165.71341125616027
-# ---------------------------------------
+    print("COMPOUND INTEREST -", compound_interest)
+    print("TOTAL AMOUNT -", amount)
