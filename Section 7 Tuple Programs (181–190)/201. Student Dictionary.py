9efd84b506c0cdea68540482f539feeef249fb3e
ANUGYA-byte <304574911+ANUGYA-byte@users.noreply.github.com>
2026-08-18 21:28:55 +0530

    Explores tuples, tuple operations, indexing, unpacking, and applications of immutable collections.

--- Section 7 Tuple Programs (181–190)/201. Student Dictionary.py
+++
@@ -1,7 +0,0 @@
-# ---------------------------------------
-# Program 201: Student Dictionary
-# Description: A dictionary storing student details as key–value pairs
-# Author: Anugya Agrawal
-# ---------------------------------------
-DICTIONARY= {"name":"John","age":20,"grade":"A"}
-print("DICTIONARY-",DICTIONARY)
