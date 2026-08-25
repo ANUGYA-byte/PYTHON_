51ce73ac18f00fde2e47675f87afc577b1d85eaa
ANUGYA-byte <304574911+ANUGYA-byte@users.noreply.github.com>
2026-08-25 19:41:04 +0530

    update

--- Section 2 Decision Making (21–40)/34. BMI Calculator.py
+++ Section 2 Decision Making (21–40)/34. BMI Calculator.py
@@ -8,16 +8,20 @@
 WEIGHT=float(input('ENTER YOUR WEIGHT IN kg-'))
 HEIGHT=float(input('ENTER YOUR HEIGHT IN m-'))
 BMI=WEIGHT/(HEIGHT**2)
-if BMI<=18.5:
+if HEIGHT <= 0 or WEIGHT <= 0:
+    print("INVALID INPUT")
+else:
+ BMI = WEIGHT / (HEIGHT ** 2)
+ if BMI<=18.5:
     print ("BMI-",BMI, 'STATUS-UNDERWEIGHT')
     
-elif 18.5<BMI<=24.9:
+ elif 18.5<BMI<=24.9:
     print ("BMI-",BMI, 'STATUS-NORMALWEIGHT')
 
-elif 24.9<BMI<=29.9:
+ elif 24.9<BMI<=29.9:
     print ("BMI-",BMI, 'STATUS-OVERWEIGHT',)
 
-elif BMI>29.9:
+ elif BMI>29.9:
     print ("BMI-",BMI, 'STATUS-OBESITY')
 
 
